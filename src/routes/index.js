f8c5ae8a212d28cff2225d8cd9a9a66ea2d7541a
tonymatheus <tony.chaves14@gmail.com>
2021-02-03 01:45:43 -0300

    Passa o contexto de logado para rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, ActivityIndicator} from 'react-native';
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
+import {AuthContext} from '../contexts/auth';
 
 function Routes() {
-  const signed = false;
+  const {signed} = useContext(AuthContext);
+
   const loading = false;
 
   if (loading) {
