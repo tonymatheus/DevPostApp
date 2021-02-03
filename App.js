f2d18eaf8fcf2f35c1ade3da3e1fe0577c88abb8
tonymatheus <tony.chaves14@gmail.com>
2021-02-03 01:47:12 -0300

    Importa gesture handler e passa contexto global para o app

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './src/routes';
+import AuthProvider from './src/contexts/auth';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar
-        backgroundColor="#36393f"
-        barStyle="light-content"
-        translucent={false}
-      />
-      <Routes />
+      <AuthProvider>
+        <StatusBar
+          backgroundColor="#36393f"
+          barStyle="light-content"
+          translucent={false}
+        />
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
