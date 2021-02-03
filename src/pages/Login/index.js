8603e9777c0b82642a86b87a60a9ed8b917bd5b7
tonymatheus <tony.chaves14@gmail.com>
2021-02-03 00:21:49 -0300

    Aplica estilos de Login e cadastro

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
+import {
+  Container,
+  ContainerInput,
+  Input,
+  Title,
+  Button,
+  ButtonText,
+  SignupButton,
+  SignupText,
+} from './styles';
 
 export default function Login() {
+  const [login, setLogin] = useState(true);
+
+  function toggleLogin() {
+    setLogin(!login);
+  }
+
+  if (login) {
+    return (
+      <Container>
+        <Title>
+          Dev<Text style={{color: '#e52246'}}>Post</Text>
+        </Title>
+        <Input
+          placeholder="Email@email.com"
+          autocapitalized="none"
+          autocorrect={false}
+        />
+        <Input
+          placeholder="senha"
+          autocapitalized="none"
+          autocorrect={false}
+          secureTextEntry={true}>
+          <Text />
+        </Input>
+        <Button onPress={() => alert('Teste')}>
+          <ButtonText>Login</ButtonText>
+        </Button>
+        <SignupButton onPress={toggleLogin}>
+          <SignupText>criar uma conta</SignupText>
+        </SignupButton>
+      </Container>
+    );
+  }
   return (
-    <View>
-      <Text>Login App</Text>
-    </View>
+    <Container>
+      <Title>
+        Dev<Text style={{color: '#e52246'}}>Post</Text>
+      </Title>
+      <Input placeholder="Nome" autocapitalized="none" autocorrect={false} />
+      <Input placeholder="email" autocapitalized="none" autocorrect={false} />
+      <Input
+        placeholder="senha"
+        autocapitalized="none"
+        autocorrect={false}
+        secureTextEntry={true}>
+        <Text />
+      </Input>
+      <Button onPress={() => alert('Teste')}>
+        <ButtonText>Cadastrar</ButtonText>
+      </Button>
+      <SignupButton onPress={toggleLogin}>
+        <SignupText>já tenho uma conta</SignupText>
+      </SignupButton>
+    </Container>
   );
 }
