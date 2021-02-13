bd203a58e1378557a57e5184668ac0916056c953
tonymatheus <tony.chaves14@gmail.com>
2021-02-13 18:56:59 -0300

    implementa dunções de login e cadastro

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,11 +1,76 @@
 import React, {useState, createContext} from 'react';
-import {Text, View} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({children}) {
+  const [user, setUser] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  const [loadingAuth, setLoadingAuth] = useState(false);
+
+  //função para login de Usuário
+  async function signIn(email, password) {
+    await auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+        //Busca de nome do usuário logado no banco
+        const userProfile = await firestore()
+          .collection('users')
+          .doc(uid)
+          .get();
+        console.log(userProfile.data().nome);
+        let data = {
+          uid: uid,
+          nome: userProfile.data().nome,
+          email: value.user.email,
+        };
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+      })
+      .catch((error) => console.log(error));
+    setLoadingAuth(false);
+  }
+
+  //função Para cadastro de usuários
+  async function signUp(email, password, name) {
+    setLoadingAuth(true);
+    await auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+        await firestore()
+          .collection('users')
+          .doc(uid)
+          .set({
+            nome: name,
+          })
+          .then(() => {
+            let data = {
+              uid: uid,
+              nome: name,
+              email: value.user.email,
+            };
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+          });
+      })
+
+      .catch((error) => console.log(error));
+    setLoadingAuth(false);
+  }
+
+  async function storageUser(data) {
+    await AsyncStorage.setItem('devApp', JSON.stringify(data));
+  }
+
   return (
-    <AuthContext.Provider value={{signed: false}}>
+    <AuthContext.Provider
+      value={{signed: !!user, user, signUp, signIn, loadingAuth}}>
       {children}
     </AuthContext.Provider>
   );
