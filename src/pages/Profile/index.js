c92d1a0a481529840b283b689fbd4da993774b61
tonymatheus <tony.chaves14@gmail.com>
2021-03-19 14:59:21 -0300

    Adionca botão de logout no profile de usuário

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, Button} from 'react-native';
+
+import {AuthContext} from '../../contexts/auth';
 
 export default function Profile() {
+  const {signOut} = useContext(AuthContext);
+
   return (
     <View>
       <Text>Profile</Text>
+      <Button onPress={() => signOut()} title="Sair" />
     </View>
   );
 }
