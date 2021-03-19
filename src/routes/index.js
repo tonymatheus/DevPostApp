e89ee675f03cb39c9f4e990edebfb0b127b13b3b
tonymatheus <tony.chaves14@gmail.com>
2021-03-19 13:45:28 -0300

    Adicoina loading para usuário logado

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,20 +5,20 @@ import AppRoutes from './app.routes';
 import {AuthContext} from '../contexts/auth';
 
 function Routes() {
-  const {signed} = useContext(AuthContext);
-
-  const loading = false;
+  const {signed, loading} = useContext(AuthContext);
 
   if (loading) {
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#36393f',
-      }}>
-      <ActivityIndicator size={50} color={'#e52246'} />
-    </View>;
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#36393f',
+        }}>
+        <ActivityIndicator size={50} color={'#e52246'} />
+      </View>
+    );
   }
 
   return signed ? <AppRoutes /> : <AuthRoutes />;
