2207d4afff7f9f4925fde40860664936019d8a37
tonymatheus <tony.chaves14@gmail.com>
2021-03-19 14:59:53 -0300

    Criar novos estilos de components

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Title = styled.Text`
+  color: black;
+  justify-content: center;
+  font-weight: bold;
+  align-items: center;
+`;
+
+export const Button = styled.TouchableOppacity`
+  color: black;
+  height: 30px;
+  width: 80px;
+  align-items: center;
+  justify-content: centar;
+`;
