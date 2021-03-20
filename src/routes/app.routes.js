c4335e27a49e0abffcc59f83879e2b962bbbbecc
tonymatheus <tony.chaves14@gmail.com>
2021-03-20 19:21:16 -0300

    Adiciona configurações de tabBar e ícones

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
+
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 import NewPost from '../pages/NewPost';
@@ -10,10 +12,43 @@ const Tab = createBottomTabNavigator();
 
 function AppRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      tabBarOptions={{
+        keyboardHidesTabBar: true,
+        showLabel: false,
+        style: {
+          backgroundColor: '#202225',
+          borderWidth: 0,
+        },
+        activeTintColor: '#fff',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Feather name="home" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Feather name="search" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Feather name="user" color={color} size={size} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
