1fecc61faf13fff01a56ce0228c195df815dc8d2
tonysousa <tony.sousa@meta.com.br>
2021-07-06 05:51:23 -0300

    Implements button to new post

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import Feather from 'react-native-vector-icons/Feather';
+import {Container, ButtonPost} from './styles';
 
 export default function Home() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <ButtonPost onPress={() => navigation.navigate('NewPost')}>
+        <Feather name="edit-2" color="#fff" size={25} />
+      </ButtonPost>
+    </Container>
   );
 }
