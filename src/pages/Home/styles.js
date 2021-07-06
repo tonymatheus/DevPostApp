10824112faac2404917e6e5254256fb868beb4c8
tonysousa <tony.sousa@meta.com.br>
2021-07-06 05:54:04 -0300

    Updates home styles  and  new post styles

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #36393f;
+`;
+
+export const ButtonPost = styled.TouchableOpacity`
+  background-color: #202225;
+  width: 60px;
+  height: 60px;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 6%;
+  right: 6%;
+  border-radius: 30px;
+`;
---
+++ src/pages/NewPost/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #404349;
+`;
+export const Input = styled.TextInput`
+  background-color: transparent;
+  margin: 10px;
+  font-size: 20px;
+  color: #fff;
+`;
+
+export const Button = styled.TouchableOpacity`
+  margin-right: 10px;
+  background-color: #418cfd;
+  border-radius: 7px;
+  padding: 5px 12px;
+  align-items: center;
+  justify-content: center;
+`;
+export const ButtonText = styled.Text`
+  font-size: 17px;
+  color: #fff;
+`;
