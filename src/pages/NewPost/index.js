97786bb68e17f19a8298f5851daa3bd387504452
tonysousa <tony.sousa@meta.com.br>
2021-07-06 05:52:11 -0300

    Implements New post Page saving data in database

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -1,10 +1,73 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useLayoutEffect, useContext} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Container, Input, ButtonText, Button} from './styles';
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
+import {AuthContext} from '../../contexts/auth';
 
 export default function NewPost() {
+  const navigation = useNavigation();
+  const [post, setPost] = useState('');
+  const {user} = useContext(AuthContext);
+  useLayoutEffect(() => {
+    const options = navigation.setOptions({
+      headerRight: () => (
+        <Button>
+          <ButtonText onPress={() => handlePost()}>Compartilhar</ButtonText>
+        </Button>
+      ),
+    });
+  }, [navigation, post]);
+
+  async function handlePost() {
+    if (post === '') {
+      console.log('Seu posto é  invalido');
+      return;
+    }
+    let avatarUrl = null;
+    try {
+      let response = await storage
+        .ref('users')
+        .child(user?.uid)
+        .getDownloadUrl();
+      avatarUrl = response;
+    } catch (error) {
+      avatarUrl = null;
+    }
+
+    await firestore()
+      .collection('posts')
+      .add({
+        created: new Date(),
+        content: post,
+        autor: user.nome,
+        likes: 0,
+        avatarUrl,
+        userId: user.uid,
+      })
+      .then(() => {
+        setPost('');
+        console.log('POST CRIADO COM SUCESSO');
+        navigation.navigate('Home');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
-    <View>
-      <Text>New Post</Text>
-    </View>
+    <Container>
+      <Input
+        placeholder="novo post"
+        placeholderTextColor="#ddd"
+        multiline={true}
+        maxLenght={300}
+        value={post}
+        onChangeText={(text) => {
+          setPost(text);
+        }}
+        autoCorrect={false}
+      />
+    </Container>
   );
 }
